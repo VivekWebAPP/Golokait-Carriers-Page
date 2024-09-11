72e8d92b2c7dd014d2a78d3d286126829f6d288b
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2024-09-12 00:09:08 +0530

    Second Commit

--- src/API/regesterUser.js
+++ src/API/regesterUser.js
@@ -2,7 +2,7 @@ const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10 MB
 
 export const UserRegistration = async (name, email, resume) => {
     try {
-        console.log('API-----> ',name,email,resume);
+        console.log('API-----> ', name, email, resume);
 
         if (resume.size > MAX_FILE_SIZE) {
             throw new Error("File size exceeds 10MB");
@@ -16,6 +16,9 @@ export const UserRegistration = async (name, email, resume) => {
         const response = await fetch('https://golaka-it-carrier-page.onrender.com/resume/upload', {
             method: "POST",
             mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
             body: formData,
         });
 
